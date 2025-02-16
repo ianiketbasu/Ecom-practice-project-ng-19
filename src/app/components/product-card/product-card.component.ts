182413c6c2621cd49cf1683fd44f6c4c5b8a2e74
Aniket Basu <aniket.basu@innofied.com>
2025-02-16 21:39:26 +0530

    Ecom practice project

---
+++ src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-product-card',
+  imports: [RouterLink],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css'
+})
+export class ProductCardComponent {
+  @Input() product: any;
+}
